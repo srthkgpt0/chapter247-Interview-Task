8dbaca990d9104eaf835a1114f6c9508a27ade62
Sarthak Gupta <sarthak.gupta@codiant.com>
2021-06-05 21:26:35 +0530

    added country state city field

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,5 +1,6 @@
-import { Button, Form, Input } from 'antd'
-import React from 'react'
+import { Button, Form, Input, message, Select, Upload } from 'antd'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useHistory } from 'react-router-dom'
 import { loginAction } from '../../Redux/auth/authSlice'
@@ -9,8 +10,50 @@ import ApiEndPoints from '../../utilities/ApiEndPoints'
 
 import { setSessionStorageToken } from '../../utilities/common'
 function SignUp() {
+  const [countryList, setCountryList] = useState([])
+  const [stateList, setStateList] = useState([])
+  const [cityList, setCityList] = useState([])
+  const [countryName, setCountryName] = useState([])
+  const [cityName, setCityName] = useState([])
+  const [stateName, setStateName] = useState([])
+
   const dispatch = useDispatch()
   const history = useHistory()
+
+  useEffect(() => {
+    getCountryData()
+  }, [])
+  const getCountryData = async () => {
+    try {
+      const res = await axios(
+        'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries.json'
+      )
+      setCountryList(res.data)
+    } catch (error) {}
+  }
+  const handleCountryChange = async (values) => {
+    try {
+      setCountryName(values[0])
+      const res = await axios(
+        'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/states.json'
+      )
+      const stateRes = res.data.filter((st) => st.country_id === values[1])
+      setStateList(stateRes)
+    } catch (error) {}
+  }
+  const handleStateChange = async (values) => {
+    try {
+      setStateName(values[0])
+      const res = await axios(
+        'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/cities.json'
+      )
+      const cityRes = res.data.filter((city) => city.state_id === values[1])
+      setCityList(cityRes)
+    } catch (error) {}
+  }
+  const handleCityChange = (values) => {
+    setCityName(values)
+  }
   const onFinish = async (values) => {
     const payload = {
       ...ApiEndPoints.signUp,
@@ -18,12 +61,32 @@ function SignUp() {
         email: values.email,
         password: values.password,
         age: values.age,
-        name: values.name
+        name: values.name,
+        state: stateName,
+        country: countryName,
+        city: cityName
       }
     }
     const res = await APIrequest(payload)
     if (res.status) {
+      message.success('Signed up successfully', 2)
       history.push('/')
+    }
+  }
+  const props = {
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text'
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList)
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`)
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`)
+      }
     }
   }
   return (
@@ -34,39 +97,85 @@ function SignUp() {
         <p>Please fill in this form to create an account.</p>
         <hr />
         <Form onFinish={onFinish}>
+          <label htmlFor='email'>
+            <b>Email</b>
+          </label>
+          <Form.Item name='email'>
+            <Input type='text' placeholder='Enter Email' required />
+          </Form.Item>
+          <label htmlFor='image'>
+            <b>Upload Profile Image</b>
+          </label>
+          <Form.Item name='image'>
+            <Upload {...props}>
+              <Button>Click to Upload</Button>
+            </Upload>
+          </Form.Item>
           <label htmlFor='name'>
             <b>Name</b>
           </label>
           <Form.Item name='name'>
-            <Input type='text' placeholder='Enter Name' name='name' required />
+            <Input type='text' placeholder='Enter Name' required />
           </Form.Item>
           <label htmlFor='age'>
             <b>Age</b>
           </label>
           <Form.Item name='age'>
-            <Input type='number' placeholder='Enter Age' name='age' required />
+            <Input type='number' placeholder='Enter Age' required />
           </Form.Item>
-          <label htmlFor='email'>
-            <b>Email</b>
+          <label htmlFor='gender'>
+            <b>Gender</b>
           </label>
-          <Form.Item name='email'>
-            <Input
-              type='text'
-              placeholder='Enter Email'
-              name='email'
-              required
-            />
+          <Form.Item name='gender'>
+            <Select>
+              <Select.Option value='male'>Male</Select.Option>
+              <Select.Option value='female'>Female</Select.Option>
+            </Select>
           </Form.Item>
-          <label htmlFor='psw'>
+          <label htmlFor='country'>
+            <b>Country</b>
+          </label>
+          <Form.Item name='country'>
+            <Select onChange={handleCountryChange}>
+              {countryList &&
+                countryList.map((cntry, index) => (
+                  <Select.Option key={index} value={[cntry.name, cntry.id]}>
+                    {cntry.name}
+                  </Select.Option>
+                ))}
+            </Select>
+          </Form.Item>
+          <label htmlFor='state'>
+            <b>Country</b>
+          </label>
+          <Form.Item name='state'>
+            <Select onChange={handleStateChange}>
+              {stateList &&
+                stateList.map((st, index) => (
+                  <Select.Option key={index} value={[st.name, st.id]}>
+                    {st.name}
+                  </Select.Option>
+                ))}
+            </Select>
+          </Form.Item>
+          <label htmlFor='city'>
+            <b>Country</b>
+          </label>
+          <Form.Item name='city'>
+            <Select onChange={handleCityChange}>
+              {cityList &&
+                cityList.map((cty, index) => (
+                  <Select.Option key={index} value={cty.name}>
+                    {cty.name}
+                  </Select.Option>
+                ))}
+            </Select>
+          </Form.Item>
+          <label htmlFor='password'>
             <b>Password</b>
           </label>
           <Form.Item name='password'>
-            <Input
-              type='password'
-              placeholder='Enter Password'
-              name='psw'
-              required
-            />
+            <Input type='password' placeholder='Enter Password' required />
           </Form.Item>
 
           <p>
